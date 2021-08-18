0fec21329b06f0f754f06e6735313803701442a9
BLADE <abdirahman.ahmed@student.moringaschool.com>
2021-08-18 21:21:46 +0300

    add form classes

---
+++ app/main/forms.py
@@ -0,0 +1,18 @@
+from flask_wtf import FlaskForm
+from flask_wtf.recaptcha import validators
+from wtforms import StringField, SelectField, TextAreaField, SubmitField
+from wtforms.validators import Required
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Write a brief bio about you.', validators = [Required()])
+    submit = SubmitField('Save')
+
+class PitchForm(FlaskForm):
+    title = StringField('Title', validators=[Required()])
+    category = SelectField('Category', choice= [('Technology', 'Technology'), ('Business', 'Business'), ('Programming', 'Programming'), ('Religion', 'Religion'), ('Sports', 'Sports')], validators=[Required()])
+    post = TextAreaField('Your pitch', validators=[Required()])
+    submit = SubmitField('Pitch')
+
+class CommentForm(FlaskForm):
+    comment = TextAreaField('Leave a comment', validators=[Required()])
+    submit = SubmitField('Comment')
