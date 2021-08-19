988088c7e06b65e29539ee60d0d96c23c11d8570
BLADE <abdirahman.ahmed@student.moringaschool.com>
2021-08-19 14:41:03 +0300

    update test

---
+++ test/user_test.py
@@ -0,0 +1,18 @@
+import unittest
+
+from app.models import User
+
+class UserTest(unittest.TestCase):
+    def setUp(self):
+        self.new_user = User(username = 'KiatuLonzo', password = '1234')
+
+    def test_password_setter(self):
+        self.assertTrue(self.new_user.password is not None)
+
+    def test_no_access_password(self):
+        with self.assertTrue(AttributeError):
+            self.new_user.password
+
+    def test_password_verification(self):
+        self.assertTrue(self.new_user.verify_password('1234'))        
+
