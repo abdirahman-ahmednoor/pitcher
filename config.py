308fd37d23122bc51334a93cc32cd4585720fe61
BLADE <abdirahman.ahmed@student.moringaschool.com>
2021-08-19 15:12:44 +0300

    setup project structure

---
+++ config.py
@@ -0,0 +1,42 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+class Config:
+    SQLALCHEMY_TRACK_MODIFICATIONS =True
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+    # simplemde configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
+    # email configuration
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIOL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+
+    # photos uplaod configuration
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI", "")
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://blade:maziwa@localhost/pitch'
+    DEBUG = True
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://blade:maziwa@localhost/pitch_test'
+
+config_options = {
+    'production':ProdConfig,
+    'development':DevConfig,
+    'test':TestConfig
+}
+
+
+                                   
