b762f21a959a854b59ce14a1b5037ca6deb55527
BLADE <abdirahman.ahmed@student.moringaschool.com>
2021-08-25 22:09:49 +0300

    add profile page

--- app/auth/views.py
+++ app/auth/views.py
@@ -22,7 +22,7 @@ def login():
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('main.html'))
+    return redirect(url_for('main.index'))
 
 @auth.route('/signup',methods = ["GET","POST"])
 def signup():
